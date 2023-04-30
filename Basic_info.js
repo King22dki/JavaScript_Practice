27cbd90e335d885c03587ab6fb44b8ebe8181fd7
Daniel King <King22dki@users.noreply.github.com>
2023-04-30 19:34:50 +0100

    completed 26 mins of a tutorial

---
+++ Basic_info.js
@@ -0,0 +1,227 @@
+// in-line comment 
+/* mult-line comment
+
+see how it can contiune as a go down to a new line
+
+see again...
+
+*/
+
+/* Data Types: In computer science, data is anything that is
+meaningful to the computer. There are seven data types in java script.
+undefined: has yet to be defined.
+null: nothing.
+boolean: True or False.
+string: any sort of text.
+symbol: An immutable primitive value that is unique
+number: is a number
+object: can store alot of different key value pairs.
+*/
+
+/* Variables:
+A variable allows computers to store and manipulate data in a dynamic fashion. 
+Declaring variables: Note only three ways of defining variables.
+*/
+var myName = "King" // I can set to other data types later. This can be used throughout your whole program.
+
+myName = 8 // here see the data type has changed.
+
+let ourName  = "King22" // another way of setting a new variable: Can only be used within the scope of your program.
+
+const pi = 3.14 // This remains constant throughout, which is evident by the name.
+
+/* Storing Values with Assigment Operator:
+
+there is a difference between declaring variables and assigning variables.
+*/
+var a; // declaring a variable
+var b = 2; // asigning a variable
+console.log(a) // console.log(a) will allow me to see what value a is at a certain point.
+a = 7; // assigining variable
+
+b = a;
+
+console.log(a) // see the values of a I will obtain when I look at the console will differ greatly. 
+
+/* Initialising Varibales with Assignment Operator
+*/
+
+var a = 9; 
+
+/* Uninitilised Variables */
+
+// Initialise these three variables
+
+var a = 5;
+var b = 10;
+var c = "I am a ";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5; 
+c = c + "String"
+
+// Case Sensitivity in Variables: just like in maths.
+
+// Declarations
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+var sum = 10 + 10;
+
+console.log(sum) // this would show you the addition
+
+var difference = 45 -23;
+
+console.log(difference)
+
+var product = 23*2;
+
+console.log(5*5)
+
+var quoient = 34/2;
+
+console.log(quoient)
+
+// Incrementing
+
+var myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1; // method 1
+myVar++; // method 2
+console.log(myVar)
+
+// Decrement
+
+var myVar = 11;
+
+// Only change code below this line
+
+myVar = myVar - 1; // method 1
+myVar--; // method 2
+
+// Floating point numbers
+
+var ourDecimal = 5.8
+
+// only change code below this line
+
+myDecimal = 0.0004
+
+// The method of multiplying and dividing a decimal is the same as it is for numbers.
+
+// Finding Remainder
+
+var remainder;
+remainder = 11 % 3; /*The remainder operator is often used to determine if a number is even or odd.
+If you can divide a number by 2 and the remainder is 0, than that means the number is even.
+*/
+
+// Compound Assignment with Augmented Addition
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Method 1
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+
+// Method 2: Augmented form
+a += 12;
+b += 9;
+c += 7; 
+
+// Compound Assignment with Augumentted subsraction
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Method 1
+a = a - 2;
+b = b - 15;
+c = c - 7; 
+
+// Method 2: Augmented form
+a -= 2;
+b -= 15;
+c -= 7;
+
+// Compound Assignment with Augumentted multiplition
+
+var a = 15
+
+// Method 1
+a = a*2;
+
+// Method 2: Augmented form
+a *= 2;
+
+// Compound Assignment with Augumentted division
+
+var b = 108;
+
+// Method 1
+b = b/4;
+
+// Method 2: Augmented form 
+b/=4;
+
+// declare String values
+
+var firstName = "Alan";
+var lastname = "Turing";
+
+// Escaping Literal Quotes in Strings
+
+var myStr = " I am a double \" double quoted\" string inside\"double quotes"
+
+//  use \ to show that they are just quotations marks - note - it won't appear on the console.
+// use ' single quotes as well
+// use ` use back tick so you can use both single and double quotes.
+
+// Escape Sequences in Strings
+
+/*
+\' single quote
+\" double quote
+\\ backslash
+
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f from feed
+
+*/
+
+var myStr = "Firstline\n\t\\SecondLine\n\ThirdLine"
+
+// Concatenating Strings with Plus Operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
