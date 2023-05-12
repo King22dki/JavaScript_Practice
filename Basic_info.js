9125c18e9a73b65cd3c819de82763daf63bf9ca2
Daniel King <King22dki@users.noreply.github.com>
2023-05-12 03:04:17 +0100

    2:47:16

--- Basic_info.js
+++ Basic_info.js
@@ -2206,5 +2206,24 @@ function freezeObj() {
         PI: 3.14
     };
 
-    
+    Object.freeze(MATH_CONSTANTS);
+    try {
+        MATH_CONSTANTS.PI =99;
+    } catch( ex ) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+
 }
+
+const PI =freezeObj();
+
+/* {stack: 'TypeError: Cannot assign to read only 
+propert…s/JavaScript_Practice/Basic_info.html:1159:11', 
+message: 'Cannot assign to read only property 'PI' of object 
+'#<Object>''}
+*/
+
+/* Use Arrow Functions to write concise Anoymous Functions
+
+*/
