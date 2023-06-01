688bbdc5401cf4daa4d335665ff06920fdda6ebf
Daniel King <King22dki@users.noreply.github.com>
2023-06-01 01:13:24 +0100

    CodeWars have begun

---
+++ New folder/CodeWars.js
@@ -0,0 +1,24 @@
+/** A Narcissitic Number (Or Armstrong Number)
+ * 
+ * A positive number which is the sum of it's own digits,
+ * rasied to the power of the number of digits in a given 
+ * base. In the first example we will look at decimal (base 10).
+ * 
+ * For example, take 153(3 digits), which is narcissistic:
+ * 
+ * 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153 
+ * 
+ * Now let's look at a number that isn't narcissistic,
+ * 1652 (4 digits):
+ * 
+ * 1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+ * 
+ * The Challenge:
+ * 
+ * Your code must return true or false (not 'true' and 'false')
+ * depending upon whether the given number is a Narcissistic number
+ * in base 10.
+ * 
+ * */
+
+
