611ab151b936dd05fba141dcf7b76292a538b1c3
Daniel King <King22dki@users.noreply.github.com>
2023-06-08 01:22:13 +0100

    making ways

---
+++ CodeWars/MaxMInList.js
@@ -0,0 +1,24 @@
+/** Maximum 
+ * This function max, recieves a list of integers as input, and
+ * return the largest number in that list. 
+ */
+
+var max = function(list){
+    var list = [];
+    list.push(list)
+
+
+    return list[0];
+}
+
+console.log(max([4,6,2,1,9,63,-134,566]))
+
+/** Minium
+ * This function min, recieves a list of integers as input, and
+ * return the lowest number in that list. 
+ */
+
+var min = function(list){
+    
+    return list[0];
+}
