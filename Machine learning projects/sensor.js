759c0dd849f2293bd3c659835808d0bbde822a73
Daniel King <King22dki@users.noreply.github.com>
2023-06-17 04:04:13 +0100

    intersection

--- Machine learning projects/sensor.js
+++ Machine learning projects/sensor.js
@@ -1,7 +1,7 @@
 class Sensor{
     constructor(car){
         this.car = car;
-        this.rayCount = 90;
+        this.rayCount = 15;
         this.rayLength = 150;
         this.raySpread = Math.PI/2; // 45 degree angle
 
