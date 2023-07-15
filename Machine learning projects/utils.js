0c6de8591755563423e1965455bc53361f5e1c13
Daniel King <King22dki@users.noreply.github.com>
2023-07-15 13:42:56 +0100

    more comments

--- Machine learning projects/utils.js
+++ Machine learning projects/utils.js
@@ -1,7 +1,9 @@
+// Linear interpolation function
 function lerp(A, B, t){
     return A+(B-A)*t;
 }
 
+// Function to calculate the intersection point between two line segments 
 function getIntersection(A, B, C, D){
     const tTop = (D.x-C.x)*(A.y-C.y)-(D.y-C.y)*(A.x-C.x);
     const uTop = (C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y);
@@ -22,6 +24,7 @@ function getIntersection(A, B, C, D){
     return null;
 }
 
+// Function to check if two polygons intersect
 function polyIntersect(poly1, poly2){
     for(let i=0; i<poly1.length; i++){
         for(let j=0; j<poly2.length; j++){
@@ -39,6 +42,7 @@ function polyIntersect(poly1, poly2){
     return false;
 }
 
+// Function to calculate RGBA color based on a value
 function getRGBA(value){
     const alpha=Math.abs(value);
     const R=value<0?0:255;
