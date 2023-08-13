2a2edac287b0dfc2080abd45b6c9f8a0ceeba92c
Daniel King <King22dki@users.noreply.github.com>
2023-08-13 23:52:54 +0100

    added a player

--- Pokemon_Like_Game/PokemonLikeGame/index.js
+++ Pokemon_Like_Game/PokemonLikeGame/index.js
@@ -17,14 +17,19 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 
 // Load an image to draw on the canvas
 const image = new Image();
-image.src = './img/Pokemon Style map 1.png'; // Replace with the actual path
+image.src = './img/Pokemon Style map 1.png';
 
-// Ensure the image is fully loaded before drawing
+// Player Image
+const playerImage = new Image();
+playerImage.src = './img/playerDown.png'; // Fix the typo here
 
+// Ensure the images are fully loaded before drawing
 image.onload = () => {
-  // Draw the image on the canvas at the specified position (x, y)
+  // Draw the background image on the canvas at the specified position (x, y)
   const x = -850; // Adjust these values as needed
   const y = -300; // Adjust these values as needed
   context.drawImage(image, x, y);
-};
 
+  // Draw the player image on the canvas at the specified position (x, y)
+  context.drawImage(playerImage, canvas.width/2, canvas.height/2);
+};
