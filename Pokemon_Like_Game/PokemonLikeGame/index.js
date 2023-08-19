0bc978169b81e57a5248d79adc73ab3c5b94cc7b
Daniel King <King22dki@users.noreply.github.com>
2023-08-20 00:11:22 +0100

    2:33:58

--- Pokemon_Like_Game/PokemonLikeGame/index.js
+++ Pokemon_Like_Game/PokemonLikeGame/index.js
@@ -14,6 +14,38 @@ for(let i = 0; i < collisions.length; i+=70){
   collisionsMap.push(collisions.slice(i, 70 + i));
 };
 
+class Boundary {
+  static width = 48;
+  static height = 48;
+  constructor({position}){
+    this.position = position;
+    this.width = 48;
+    this.height = 48;
+  }
+
+  draw() {
+    context.fillStyle = 'red';
+    context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const boundaries = [];
+
+collisionsMap.forEach((row, i) =>{
+  row.forEach((symbol, j)=> {
+    if(symbol === 1025)
+    boundaries.push(
+      new Boundary({
+        position: {
+          x: j * Boundary.width,
+          y: i * Boundary.height
+        }
+      })
+    )
+  })
+})
+
+console.log(boundaries);
 // Load an image to draw on the canvas
 const image = new Image();
 image.src = './img/Pokemon Style map 1.png';
@@ -129,6 +161,11 @@ window.addEventListener('keyup', (e) => {
       break;
   }
 });
+
+
+
+
+
 
 
 
